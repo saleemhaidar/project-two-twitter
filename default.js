319f78b4a946002946b56116105792798fc3d595
Saleem Haidar <saleemhaidar@gmail.com>
2016-06-28 13:02:08 -0700

    User can view a user timeline all tweets in one block #1

--- default.js
+++ default.js
@@ -1,8 +1,45 @@
-function User(name, tweets) {
+function User(name, tweets, image) {
   this.name = name;
-  this.tweet = tweet;
+  this.tweets = tweets;
+  this.image = image;
 }
 
-var jon = new User('John', ['Tweet 1', 'Tweet 2', 'Tweet 3']);
+var john = new User('John', ['Go Lakers.', 'It is raining.', 'Good night.'], 'images/John.png');
 var steph = new User('Steph', ['Tweet 1', 'Tweet 2','Tweet 3']);
-var james = new User('Steph', ['Tweet 1', 'Tweet 2','Tweet 3']);
+var james = new User('James', ['Tweet 1', 'Tweet 2','Tweet 3']);
+
+
+var userPage = document.getElementById('userPage');
+
+var container = document.createElement('div');
+container.setAttribute('class', 'col-md-6 col-md-offset-3');
+
+var subContainer = document.createElement('div');
+subContainer.setAttribute('class', 'panel panel-default');
+
+var user =document.createElement('div');
+user.setAttribute('class', 'panel-body');
+
+var theName = document.createElement('h3');
+theName.textContent = john.name
+
+var theImage = document.createElement('img');
+theImage.setAttribute('src', john.image);
+theImage.setAttribute('class', 'img-responsive col-md-3' )
+
+theTweets = document.createElement('h5');
+theTweets.textContent = john.tweets
+
+document.body.appendChild(userPage);
+userPage.appendChild(container);
+container.appendChild(subContainer);
+subContainer.appendChild(user);
+user.appendChild(theName);
+user.appendChild(theImage);
+theName.appendChild(theTweets);
+
+
+
+// window.addEventListener('load', function(e) {
+//
+// })
