b447da3dd933e763f31d1978e05397f7c27a8deb
Saleem Haidar <saleemhaidar@gmail.com>
2016-06-28 09:50:42 -0700

    Created a User Object #1

---
+++ default.js
@@ -0,0 +1,8 @@
+function User(name, tweets) {
+  this.name = name;
+  this.tweet = tweet;
+}
+
+var jon = new User('John', ['Tweet 1', 'Tweet 2', 'Tweet 3']);
+var steph = new User('Steph', ['Tweet 1', 'Tweet 2','Tweet 3']);
+var james = new User('Steph', ['Tweet 1', 'Tweet 2','Tweet 3']);
