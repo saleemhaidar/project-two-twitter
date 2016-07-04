27c1bec4ca9352281812756b0879f274c8c771ac
Saleem Haidar <saleemhaidar@gmail.com>
2016-07-03 18:48:06 -0700

    User can view a user's timeline modified #1

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var allTweets = [
+var allTweets = {
+    john: {name: 'John', tweets: ['Go Lakers.', 'It is raining.', 'Good night.'], image: 'images/John.png'},
+    peter: {name: 'Peter', tweets: ['Tweet 1.', 'Tweet 2.', 'Tweet 3.'], image: 'images/peter-griffin.jpeg'},
+    homer: {name: 'Homer', tweets: ['Tweet a.', 'Tweet b.', 'Tweet c.'], image: 'images/homer-simpson.gif-c200'}
+    // {user: 'Steph', tweet: 'Tweet 1.'},
+    // {user: 'John', tweet: 'It is raining'},
+    // {user: 'James', tweet: 'Tweet a.'},
+    // {user: 'James', tweet: 'Tweet b.'},
+    // {user: 'John', tweet: 'Good night.'},
+    // {user: 'Steph', tweet: 'Tweet 2.'},
+    // {user: 'Steph', tweet: 'Tweet 3.'},
+    // {user: 'James', tweet: 'Tweet c.'},
 
-    {user: 'John', tweet: 'Go Lakers.'},
-    {user: 'John', tweet: 'Go Lakers.'},
-    {user: 'Steph', tweet: 'Tweet 1.'},
-    {user: 'John', tweet: 'It is raining'},
-    {user: 'James', tweet: 'Tweet a.'},
-    {user: 'James', tweet: 'Tweet b.'},
-    {user: 'John', tweet: 'Good night.'},
-    {user: 'Steph', tweet: 'Tweet 2.'},
-    {user: 'Steph', tweet: 'Tweet 3.'},
-    {user: 'James', tweet: 'Tweet c.'},
+}
 
-]
+app.use(express.static('./'))
 
 app.get('/tweets/:user', function(req, res){
   var matched = [];
@@ -23,6 +25,11 @@ app.get('/tweets/:user', function(req, res){
     }
   });
   res.json(matched);
+});
+
+app.get('/data', function(req, res){
+
+  res.send(allTweets.john);
 });
 
 app.listen(8080);
--- user.js
+++ user.js
@@ -1,57 +1,42 @@
-function User(name, tweets, image) {
-  this.name = name;
-  this.tweets = tweets;
-  this.image = image;
-}
 
-var john = new User('John', ['Go Lakers.', 'It is raining.', 'Good night.'], 'images/John.png');
-var steph = new User('Steph', ['Tweet 1', 'Tweet 2','Tweet 3']);
-var james = new User('James', ['Tweet a', 'Tweet b','Tweet c']);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/data');
+  xhr.send();
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.responseText);
+    console.log(response);
 
-var allUsers = [
-  john, steph, james
-];
+    for (var i = 0; i < response.tweets.length; i++) {
+      var thisTweet = response.tweets[i];
+      var userPage = document.getElementById('userPage');
 
-// for (var i = 0; i < allUsers.length; i++) {
-//   var thisUser = allUsers[i];
-  showUser(john);
+      var container = document.createElement('div');
+      container.setAttribute('class', 'col-md-6 col-md-offset-3');
 
+      var subContainer = document.createElement('div');
+      subContainer.setAttribute('class', 'panel panel-default');
 
-function showUser(theUser){
+      var user = document.createElement('div');
+      user.setAttribute('class', 'panel-body');
 
+      var theName = document.createElement('h3');
+      theName.setAttribute('class', 'name')
+      theName.textContent = response.name
 
-  for (var i = 0; i < theUser.tweets.length; i++) {
+      var theImage = document.createElement('img');
+      theImage.setAttribute('src', response.image);
+      theImage.setAttribute('class', 'img-responsive col-md-3' )
 
-    var thisTweet = theUser.tweets[i];
-    var userPage = document.getElementById('userPage');
+      theTweets1 = document.createElement('span');
+      theTweets1.setAttribute('class', 'col-md-6');
+      theTweets1.textContent = thisTweet;
 
-    var container = document.createElement('div');
-    container.setAttribute('class', 'col-md-6 col-md-offset-3');
-
-    var subContainer = document.createElement('div');
-    subContainer.setAttribute('class', 'panel panel-default');
-
-    var user =document.createElement('div');
-    user.setAttribute('class', 'panel-body');
-
-    var theName = document.createElement('h3');
-    theName.setAttribute('class', 'name')
-    theName.textContent = theUser.name
-
-    var theImage = document.createElement('img');
-    theImage.setAttribute('src', theUser.image);
-    theImage.setAttribute('class', 'img-responsive col-md-3' )
-
-    theTweets1 = document.createElement('span');
-    theTweets1.setAttribute('class', 'col-md-6');
-    theTweets1.textContent = thisTweet;
-
-    document.body.appendChild(userPage);
-    userPage.appendChild(container);
-    container.appendChild(subContainer);
-    subContainer.appendChild(user);
-    user.appendChild(theName);
-    user.appendChild(theImage);
-    user.appendChild(theTweets1);
-  }
-}
+      document.body.appendChild(userPage);
+      userPage.appendChild(container);
+      container.appendChild(subContainer);
+      subContainer.appendChild(user);
+      user.appendChild(theName);
+      user.appendChild(theImage);
+      user.appendChild(theTweets1);
+    }
+  });
